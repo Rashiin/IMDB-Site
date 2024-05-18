e6186c7fac10fe60a4eb33b5bceeeffad1c0688a
Rashin Gholijani Farahani <111678433+Rashiin@users.noreply.github.com>
2024-05-18 15:43:09 +0330

    Update Header.js

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
     const [isOpen, setIsOpen] = useState(false);
 
     useEffect(() => {
-        axios.get('https://rashiin.github.io/api/db.json')
+        axios.get('http://localhost:3002/menu')
             .then(response => {
                 if (Array.isArray(response.data)) {
                     setMenuItems(response.data);
